4048058b7c53fded48e0dd0a2e80125a69fdcc14
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2025-03-27 16:41:28 +0900

    Do not add `\n`

--- src/DocstringTranslation.jl
+++ src/DocstringTranslation.jl
@@ -39,8 +39,8 @@ function postprocess_content(content::AbstractString)
     # Replace each match with the text wrapped in a math code block
     return replace(
         content, 
-        r":\$(.*?):\$"s => s"```math\n\1\n```",
-        r"\$\$(.*?)\$\$"s => s"```math\n\1\n```"
+        r":\$(.*?):\$"s => s"```math\1```",
+        r"\$\$(.*?)\$\$"s => s"```math\1```"
         )
 end
 
